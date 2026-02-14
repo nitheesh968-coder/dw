a79d5df60b9d3de27aa3bdf6d6586807f4e05dfa
nitheesh968-coder <nitheesh968@gmail.com>
2026-02-14 15:16:22 +0530

    Update app.js

--- public/portal/app.js
+++ public/portal/app.js
@@ -97,7 +97,7 @@ addMessage(data.text, true)
   });
 
 socket.on("connect_update", ({status, userId}) => {
-  if(userId === user_name) return;
+  if(userId !== user_name) return;
   console.log(user_name,userId,"Connect")
 document.getElementById("status_name").textContent = status;
 document.getElementById("status_icon").style.background = "green";
